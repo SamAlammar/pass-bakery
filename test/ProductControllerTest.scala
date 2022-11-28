3025430232e252c4ec3dfd3e7473e53f5ece7547
SamAlammar <60335436+SamAlammar@users.noreply.github.com>
2022-11-28 16:06:39 -0600

    Improved tests as discussed

--- test/ProductControllerTest.scala
+++ test/ProductControllerTest.scala
@@ -5,7 +5,6 @@ import scala.concurrent.Future
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Environment
-import play.api.libs.Comet.json
 import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.mvc._
 import play.api.test._
@@ -40,20 +39,26 @@ class ProductControllerTest extends PlaySpec
     }
   }
 
-  "GET <root>/rest/bakery/product/:id test" should{
-    "be valid" in {
-      val environment = Environment.simple()
+  "GET <root>/rest/bakery/product/:id" should{
+    val environment = Environment.simple()
 
-      val controller = new ProductController(Helpers.stubControllerComponents(), environment)
-      val result: Future[Result] = controller.getInventory().apply(FakeRequest())
-      val json : JsValue = Json.parse(contentAsString(result))
+    val controller = new ProductController(Helpers.stubControllerComponents(), environment)
+    val result: Future[Result] = controller.getInventory().apply(FakeRequest())
+    val json: JsValue = Json.parse(contentAsString(result))
 
-      val existingId = (json.as[Seq[JsObject]].head \ "id").as[String]
-      val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
-      val invalidId = ""
+    val existingId = (json.as[Seq[JsObject]].head \ "id").as[String]
+    val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
+    val invalidId = ""
 
+    "existing id test be valid" in {
       status(controller.getProduct(existingId).apply(FakeRequest())) mustBe 200
+    }
+
+    "none existing id test be valid" in{
       status(controller.getProduct(noneId).apply(FakeRequest())) mustBe 404
+    }
+
+    "invalid id test be valid" in {
       status(controller.getProduct(invalidId).apply(FakeRequest())) mustBe 404
     }
   }
@@ -77,59 +82,71 @@ class ProductControllerTest extends PlaySpec
     }
   }
 
-  "PUT <root>/rest/bakery/product/:id test" should {
-    "be valid" in {
-      val environment = Environment.simple()
-      val controller = new ProductController(Helpers.stubControllerComponents(), environment)
+  "PUT <root>/rest/bakery/product/:id" should {
+    val environment = Environment.simple()
+    val controller = new ProductController(Helpers.stubControllerComponents(), environment)
 
-      val result: Future[Result] = controller.getInventory().apply(FakeRequest())
-      val json: JsValue = Json.parse(contentAsString(result))
-      val element = json.as[Seq[JsObject]].head
+    val result: Future[Result] = controller.getInventory().apply(FakeRequest())
+    val json: JsValue = Json.parse(contentAsString(result))
+    val element = json.as[Seq[JsObject]].head
 
-      val existingId = (element \ "id").as[String]
-      val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
-      val invalidId = ""
+    val existingId = (element \ "id").as[String]
+    val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
+    val invalidId = ""
 
-      val requestJson = Json.obj(
-        "name" -> (element \ "name").as[String],
-        "quantity" -> ((element \ "quantity").as[Int] + 1),
-        "price" -> ((element \ "price").as[Double] + 1)
-      )
+    val requestJson = Json.obj(
+      "name" -> (element \ "name").as[String],
+      "quantity" -> ((element \ "quantity").as[Int] + 1),
+      "price" -> ((element \ "price").as[Double] + 1)
+    )
 
-      val requestGood = FakeRequest(PUT, "rest/bakery/product/"+existingId).withHeaders(CONTENT_TYPE -> "application/json")
-        .withBody(requestJson)
+    val requestGood = FakeRequest(PUT, "rest/bakery/product/"+existingId).withHeaders(CONTENT_TYPE -> "application/json")
+      .withBody(requestJson)
 
-      val requestNoId = FakeRequest(PUT, "rest/bakery/product/" + noneId).withHeaders(CONTENT_TYPE -> "application/json")
-        .withBody(requestJson)
+    val requestNoId = FakeRequest(PUT, "rest/bakery/product/" + noneId).withHeaders(CONTENT_TYPE -> "application/json")
+      .withBody(requestJson)
 
-      val requestInvalid = FakeRequest(PUT, "rest/bakery/product/" + invalidId).withHeaders(CONTENT_TYPE -> "application/json")
-        .withBody(requestJson)
+    val requestInvalid = FakeRequest(PUT, "rest/bakery/product/" + invalidId).withHeaders(CONTENT_TYPE -> "application/json")
+      .withBody(requestJson)
 
-      val resultGood = call(controller.updateProduct(existingId), requestGood)
-      val resultNoId = call(controller.updateProduct(noneId), requestNoId)
-      val resultInvalid = call(controller.updateProduct(invalidId), requestInvalid)
+    val resultGood = call(controller.updateProduct(existingId), requestGood)
+    val resultNoId = call(controller.updateProduct(noneId), requestNoId)
+    val resultInvalid = call(controller.updateProduct(invalidId), requestInvalid)
 
+    "existing id test be valid" in {
       status(resultGood) mustBe 200
+    }
+
+    "none existing id test be valid" in {
       status(resultNoId) mustBe 404
+    }
+
+    "Invalid id test be valid" in {
       status(resultInvalid) mustBe 404
     }
   }
 
   "DELETE <root>/rest/bakery/product/:id test" should {
-    "be valid" in {
-      val environment = Environment.simple()
-      val controller = new ProductController(Helpers.stubControllerComponents(), environment)
+    val environment = Environment.simple()
+    val controller = new ProductController(Helpers.stubControllerComponents(), environment)
 
-      val result: Future[Result] = controller.getInventory().apply(FakeRequest())
-      val json: JsValue = Json.parse(contentAsString(result))
-      val element = json.as[Seq[JsObject]].head
+    val result: Future[Result] = controller.getInventory().apply(FakeRequest())
+    val json: JsValue = Json.parse(contentAsString(result))
+    val element = json.as[Seq[JsObject]].head
 
-      val existingId = (element \ "id").as[String]
-      val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
-      val invalidId = ""
+    val existingId = (element \ "id").as[String]
+    val noneId = existingId.replace(existingId.charAt(0), (existingId.charAt(0).toInt - 1).toChar)
+    val invalidId = ""
 
+    "existing id test be valid" in {
       status(controller.deleteProduct(existingId).apply(FakeRequest())) mustBe 200
+    }
+
+    "none existing id test be valid" in {
       status(controller.deleteProduct(noneId).apply(FakeRequest())) mustBe 404
+    }
+
+    "Invalid id test be valid" in {
       status(controller.deleteProduct(invalidId).apply(FakeRequest())) mustBe 404
     }
   }
